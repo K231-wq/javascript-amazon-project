9f68583222e672927c1feae302c6694b3e82041d
K231-wq <minthetkhaing.013@gmail.com>
2024-10-10 23:04:43 +0630

    Create Js generator for amazon grid products

---
+++ scripts/amazon.js
@@ -0,0 +1,70 @@
+
+const productGrid = document.querySelector('.js-products-grid');
+
+let productsListHtml = ' ';
+
+renderHtml();
+
+function renderHtml(){
+    
+    products.forEach((product, index) => {
+        console.log(product.rating.star);
+        productsListHtml += `
+                <div class="product-container">
+          <div class="product-image-container">
+            <img class="product-image"
+              src="${product.image}">
+          </div>
+
+          <div class="product-name limit-text-to-2-lines">
+            ${product.title}
+          </div>
+
+          <div class="product-rating-container">
+            <img class="product-rating-stars"
+              src="images/ratings/rating-${product.rating.stars*10}.png">
+            <div class="product-rating-count link-primary">
+              ${product.rating.count}
+            </div>
+          </div>
+
+          <div class="product-price">
+            ${priceCents(product.priceCents)}
+          </div>
+
+          <div class="product-quantity-container">
+            <select>
+              <option selected value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+
+          <div class="product-spacer"></div>
+
+          <div class="added-to-cart">
+            <img src="images/icons/checkmark.png">
+            Added
+          </div>
+
+          <button class="add-to-cart-button button-primary">
+            Add to Cart
+          </button>
+        </div>
+        `;
+    });
+    productGrid.innerHTML = productsListHtml;
+}
+console.log(productsListHtml);
+
+//small function
+function priceCents(price){
+    return (price/100).toFixed(2);
+};
