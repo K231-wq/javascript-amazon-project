d2aa46c75ec7bc90757fb5c4856ae0e68da6a14a
K231-wq <minthetkhaing.013@gmail.com>
2024-10-16 13:56:07 +0630

    testing framework

---
+++ test-jasmin/testMoney.js
@@ -0,0 +1,13 @@
+import { formatPrice } from "../scripts/util/formatPriceCents.js";
+
+describe('test suite: format currency', () => {
+    it('converts cents into dollars', () => {
+        expect(formatPrice(2095)).toEqual('20.95');
+    });
+    it('works with 0', () => {
+        expect(formatPrice(0)).toEqual('0.00');
+    });
+    it('rounds up to the nearest cents', () => {
+        expect(formatPrice(2000.5)).toEqual('20.01');
+    })
+});
